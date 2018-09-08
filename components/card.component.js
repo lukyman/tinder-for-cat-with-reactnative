f49035162424a0f36a82a215f3963d7c3112f5dc
mzawadi <tech@mzawadi.com>
2018-09-08 15:02:38 +0300

    create card component

---
+++ components/card.component.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Card} from 'react-native-elements';
+
+export default class CardComponent extends React.Component {
+render(){
+    return (
+        <Card title="Cat"/>
+    );
+}
+} 
